354e18e449b35c6b114e5a76b4ecb921bb40e618
craig91 <craigdejean1@gmail.com>
2017-05-17 23:39:53 -0400

    added server file, package.json, stored old files in old folder

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyparser = require('body-parser');
+const router = require('routes/index.js');
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(express.static('./public'));
+app.use('/api', router)
+app.get('./*', (req, res) =>
+   res.sendFile(path.join(__dirname, './public/index.html'))
+)
+
+app.listen(3000,() => {
+    console.log('Server Started!')
+});
