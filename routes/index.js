1a2a5a574a9586e68b6f882ef2d4198cac554657
craig91 <craigdejean1@gmail.com>
2017-05-26 13:37:24 -0400

    deleted blogs-list route && added /blogs route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-// router.use('/blogs-list', require('./blogs_Route.js'));
+router.use('/blogs', require('./blogs_Route.js'));
 // router.use('/projects-list', require('./projects_Route.js'));
 
 module.exports = router;
