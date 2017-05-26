df696cd507aa00224d3982524a6c6184a142dc2a
craig91 <craigdejean1@gmail.com>
2017-05-26 13:40:07 -0400

    added react router, css files, 2 new components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { IndexRoute, Link, Router, Route, browserHistory, hashHistory } from 'react-router';
 
+import '../css/_sideBar.css';
+import '../css/_main.css';
+
+import SideBar from '../components/side_bar.jsx';
+import BlogWrapper from '../components/blog_list_wrapper.jsx';
+
 
 class App extends React.Component {
   constructor(props) {
@@ -10,14 +16,23 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        Hello
+        <SideBar />
+        {this.props.children}
       </div>
     )
   }
 }
 
 
+
 ReactDOM.render (
-  <App />,
+  <Router history={browserHistory}>
+    <Route path='/' component={App}>
+      <IndexRoute component={BlogWrapper} />
+      {/* <Route path='/list' component={BlogList} />
+      <Route path='/login' component={LoginForm} />
+      <Route path='/distros' component={Distros} /> */}
+    </Route>
+  </Router>,
   document.getElementById('root')
-)
+);
