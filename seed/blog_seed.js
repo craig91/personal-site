e2b77855c4e38ef59b953fa2a95f329860d424ff
craig91 <craigdejean1@gmail.com>
2017-05-28 21:35:53 -0400

    added a blog id

--- seed/blog_seed.js
+++ seed/blog_seed.js
@@ -7,19 +7,22 @@ Blog.sync({force: true})
     hero:'https://malcoded.com/wp-content/uploads/2016/11/angularLogo-large.png',
     title:'Next endeavor! Learning Angular 2 pt.2',
     shortDescript:'Every week I will be telling you guys about learning Angular 2 on my own!',
-    post:'something'
+    post:'something',
+    blogId: 1
   },
   {
     hero:'https://malcoded.com/wp-content/uploads/2016/11/angularLogo-large.png',
     title:'Next endeavor! Learning Angular 2 pt.3',
     shortDescript:'Every week I will be telling you guys about learning Angular 2 on my own!',
-    post:'something'
+    post:'something',
+    blogId: 2
   },
   {
     hero:'https://malcoded.com/wp-content/uploads/2016/11/angularLogo-large.png',
     title:'Next endeavor! Learning Angular 2 pt.4',
     shortDescript:'Every week I will be telling you guys about learning Angular 2 on my own!',
-    post:'something'
+    post:'something',
+    blogId: 3
   },
 ]))
 .catch((err) => console.log(err));
