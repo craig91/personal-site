ff3a1f8bc1c4f1c8facc8b428788a436481993ce
craig91 <craigdejean1@gmail.com>
2017-05-29 23:11:13 -0400

    organized code

--- components/blog_list.jsx
+++ components/blog_list.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import $ from 'jquery';
-import { Link } from 'react-router';
+import {Link} from 'react-router';
 import BlogPage from './blog_page.jsx';
-
+import SideBar from './side_bar.jsx';
 
 class BlogList extends React.Component {
   constructor(props) {
@@ -12,30 +12,31 @@ class BlogList extends React.Component {
     };
   }
   componentDidMount() {
-    $.ajax({
-      url: '/api/blogs',
-      type: 'GET'
-    }).done((data) => {
+    $.ajax({url: '/api/blogs', type: 'GET'}).done((data) => {
       console.log(data)
-      this.setState({ blogs: data })
+      this.setState({blogs: data})
     })
   }
   render() {
     return (
-      <div>{this.state.blogs.length === 0 ? "Loading.." : this.state.blogs.map(function(val, idx) {
-        return(
-            <ol key={idx} className="posts">
-              <li>
-                <Link to={"/blogs/ + val.id"} className="post-title" href="">{val.title}</Link>
-                <div className="descript">{val.shortDescript}
-                </div>
-                <span className="date">
-                  Date Posted: May 8th, 2017</span>
-              </li>
-            </ol>
-        )
-      })}
+      <div>
+        <div>{this.state.blogs.length === 0
+            ? "Loading.."
+            : this.state.blogs.map(function(val, idx) {
+              return (
+                <ol key={idx} className="posts">
+                  <li>
+                    <Link to={"/blogs/ + val.id"} className="post-title" href="">{val.title}</Link>
+                    <div className="descript">{val.shortDescript}
+                    </div>
+                    <span className="date">
+                      Date Posted: May 8th, 2017</span>
+                  </li>
+                </ol>
+              )
+            })}
 
+        </div>
       </div>
     )
   }
