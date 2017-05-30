17ea88ae292f1729cbbdebbe2a6996ac26160964
craig91 <craigdejean1@gmail.com>
2017-05-30 17:56:05 -0400

    fixed Link issue

--- components/blog_list.jsx
+++ components/blog_list.jsx
@@ -20,13 +20,16 @@ class BlogList extends React.Component {
   render() {
     return (
       <div>
+       <div>
+          <SideBar />
+       </div>
         <div>{this.state.blogs.length === 0
             ? "Loading.."
             : this.state.blogs.map(function(val, idx) {
               return (
                 <ol key={idx} className="posts">
                   <li>
-                    <Link to={"/blogs/ + val.id"} className="post-title" href="">{val.title}</Link>
+                    <Link to={"/blogs/" + val.id} className="post-title" href="">{val.title}</Link>
                     <div className="descript">{val.shortDescript}
                     </div>
                     <span className="date">
