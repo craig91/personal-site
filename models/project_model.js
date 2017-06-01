c38a189593c82ba94964be45df3491532b544bc8
craig91 <craigdejean1@gmail.com>
2017-06-01 15:05:28 -0400

    added model for projects page

---
+++ models/project_model.js
@@ -0,0 +1,26 @@
+'use strict'
+module.exports = function(sequelize, DataTypes) {
+  const Project = sequelize.define('Python', {
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isAlpha: true
+    },
+    DemoUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isAlpha: true
+    },
+    gitUrl: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isAlpha: true
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isAlpha: true
+    }
+  })
+  return Project;
+}
