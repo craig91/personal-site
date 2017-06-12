bb41f09080d5fbb45f6d91945b4956810aa79456
Craig Dejean <thelegendarycraig@yahoo.com>
2017-06-12 01:28:39 -0400

    changed some text in description

--- seed/blog_seed.js
+++ seed/blog_seed.js
@@ -7,21 +7,21 @@ Blog.sync({force: true})
     hero:'https://malcoded.com/wp-content/uploads/2016/11/angularLogo-large.png',
     title:'Next endeavor! Learning Angular 2 pt.1',
     shortDescript:'Every week I will be telling you guys about learning Angular 2 on my own!',
-    post:'something',
+    post:'PART 1',
     blogId: 1
   },
   {
     hero:'https://malcoded.com/wp-content/uploads/2016/11/angularLogo-large.png',
     title:'Next endeavor! Learning Angular 2 pt.2',
     shortDescript:'Every week I will be telling you guys about learning Angular 2 on my own!',
-    post:'something',
+    post:'PART 2',
     blogId: 2
   },
   {
     hero:'https://malcoded.com/wp-content/uploads/2016/11/angularLogo-large.png',
     title:'Next endeavor! Learning Angular 2 pt.3',
     shortDescript:'Every week I will be telling you guys about learning Angular 2 on my own!',
-    post:'something',
+    post:'PART 3',
     blogId: 3
   },
 ]))
