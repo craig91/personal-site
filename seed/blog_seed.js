fb2d96dbf5f4d834f324814021dfe375bf6274ad
Craig Dejean <thelegendarycraig@yahoo.com>
2017-06-27 16:19:27 -0400

    Changes image URL for Angular 2

--- seed/blog_seed.js
+++ seed/blog_seed.js
@@ -4,21 +4,21 @@ const Blog = require('../models').Blog;
 Blog.sync({force: true})
 .then(() => Blog.bulkCreate([
   {
-    hero:'https://malcoded.com/wp-content/uploads/2016/11/angularLogo-large.png',
+    hero:'https://i.ytimg.com/vi/XlqoPpLMdwY/maxresdefault.jpg',
     title:'Next *endeavor*! Learning Angular 2 pt.1',
     shortDescript:'Every week I will be telling you guys about learning **Angular 2** on my own!',
     post:'PART 1',
     blogId: 1
   },
   {
-    hero:'https://malcoded.com/wp-content/uploads/2016/11/angularLogo-large.png',
+    hero:'https://i.ytimg.com/vi/XlqoPpLMdwY/maxresdefault.jpg',
     title:'Next endeavor! Learning Angular 2 pt.2',
     shortDescript:'Every week I will be telling you guys about learning Angular 2 on my own!',
     post:'PART 2',
     blogId: 2
   },
   {
-    hero:'https://malcoded.com/wp-content/uploads/2016/11/angularLogo-large.png',
+    hero:'https://i.ytimg.com/vi/XlqoPpLMdwY/maxresdefault.jpg',
     title:'Next endeavor! Learning Angular 2 pt.3',
     shortDescript:'Every week I will be telling you guys about learning Angular 2 on my own!',
     post:'PART 3',
