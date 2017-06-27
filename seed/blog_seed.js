76868b09f1a84f64dcaab1bfe629aa3901cb2914
Craig Dejean <thelegendarycraig@yahoo.com>
2017-06-27 13:42:40 -0400

    added in test markdown symbols

--- seed/blog_seed.js
+++ seed/blog_seed.js
@@ -5,8 +5,8 @@ Blog.sync({force: true})
 .then(() => Blog.bulkCreate([
   {
     hero:'https://malcoded.com/wp-content/uploads/2016/11/angularLogo-large.png',
-    title:'Next endeavor! Learning Angular 2 pt.1',
-    shortDescript:'Every week I will be telling you guys about learning Angular 2 on my own!',
+    title:'Next *endeavor*! Learning Angular 2 pt.1',
+    shortDescript:'Every week I will be telling you guys about learning **Angular 2** on my own!',
     post:'PART 1',
     blogId: 1
   },
