10ca10a65db5233e5e1ae363c9d1660bb62d0215
Craig Dejean <thelegendarycraig@yahoo.com>
2017-06-30 20:39:44 -0400

    Deleted <p> and attempted inline stying using linear gradient

--- components/blog_page.jsx
+++ components/blog_page.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import $ from 'jquery';
 
+const DivStyle = {
+   backgroundSize: 'cover',
+   backgroundColor: 'rgba(0, 0, 0, 0.3)'
+}
+
 class BlogPage extends React.Component {
   constructor(props) {
     super(props)
@@ -23,14 +28,20 @@ class BlogPage extends React.Component {
       console.log(blog)
       return (
         <div>
-          <div className="overlay">
-          {/*<div className="banner" style={{
-            backgroundImage: `url(${this.state.blogs.hero})`
-          }}></div>*/}
-          {/*<p className="test">hello world</p>*/}
-           <img src={this.state.blogs.hero} />
-           
-          </div>
+          <header className="gradient" style={{
+              backgroundImage: `url(${this.state.blogs.hero})`, DivStyle 
+           }} >
+            <div className="head">
+              
+              
+            </div>
+          </header>
+
+          <main className="blog-page-main">
+            <h2 className="heading">Information about Cheese Curds</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa facilis, labore debitis nesciunt ipsam odio corporis architecto reiciendis voluptas ab quibusdam ratione rerum tempore voluptatibus libero cumque dignissimos vero hic!</p>
+          </main>
+
           <div className="content">{this.state.blogs.post}</div>
         </div>
       )
@@ -42,5 +53,13 @@ class BlogPage extends React.Component {
   }
 }
 
+// <div className="overlay">
+// {/*<div className="banner" style={{
+//   backgroundImage: `url(${this.state.blogs.hero})`
+// }}></div>*/}
+// {/*<p className="test">hello world</p>*/}
+//  <img src={this.state.blogs.hero} />
+//
+// </div>
 
 export default BlogPage;
